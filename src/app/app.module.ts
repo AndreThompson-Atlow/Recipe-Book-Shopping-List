44022c10c1ac73498feffa587f99e079ea2762f3
andre thompson <andrethompsoncs@gmail.com>
2021-02-01 12:37:47 -0700

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ShoppinglistComponent } from './components/shoppinglist/shoppinglist/shoppinglist.component';
+import { ShoppinglisteditComponent } from './components/shoppinglist/shoppinglistedit/shoppinglistedit.component';
+import { RecipelistComponent } from './components/recipebook/recipelist/recipelist.component';
+import { RecipeitemComponent } from './components/recipebook/recipeitem/recipeitem.component';
+import { RecipedetailComponent } from './components/recipebook/recipedetail/recipedetail.component';
+import { RecipeComponent } from './components/recipebook/recipe/recipe.component';
+import { IngredientComponent } from './components/shoppinglist/ingredient/ingredient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppinglistComponent,
+    ShoppinglisteditComponent,
+    RecipelistComponent,
+    RecipeitemComponent,
+    RecipedetailComponent,
+    RecipeComponent,
+    IngredientComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, CommonModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
